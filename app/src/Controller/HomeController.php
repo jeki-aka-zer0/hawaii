dfbff33daf48144994c3b86b8af93e520be7104e
Evgeniy Zhukov <zer0.stat@mail.ru>
2021-04-21 21:43:34 +0300

    Symfony website skeleton added

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -5,23 +5,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @RequiresUserCredits()
- */
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
-     * @return JsonResponse
+     * @return Response
      */
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return new JsonResponse([
-            'name' => 'Pet project of Evgeniy Zhukov ',
-            'version' => '1.0',
-        ]);
+        return $this->render('app/home.html.twig');
     }
 }
