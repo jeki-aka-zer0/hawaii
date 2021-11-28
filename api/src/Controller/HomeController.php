53e9dc177f9fadd2808916e6faf9cf891161e033
Evgeniy Zhukov <zer0.stat@mail.ru>
2021-11-28 20:34:43 +0300

    refactor(API): annotations added
    Switch route declarations from routes.yaml to dockBlocks

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController
 {
+    /**
+     * @Route("/", name="home")
+     * @return Response
+     */
     public function index(): Response
     {
         return new Response('Yo');
