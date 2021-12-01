30337dc1963546ece183563c06d1df69b5967532
Evgeniy Zhukov <zer0.stat@mail.ru>
2021-12-01 22:37:59 +0300

    feat(Tests): add PHPUnit

---
+++ api/tests/Unit/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyTest extends TestCase
+{
+    public function testShouldSuccess(): void
+    {
+        self::assertTrue(true);
+    }
+}
