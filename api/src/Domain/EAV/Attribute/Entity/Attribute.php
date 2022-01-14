11b36850962c5d7d87a408585b9114ca1c86dab3
Evgeniy Zhukov <zer0.stat@mail.ru>
2022-01-15 00:22:13 +0300

    refactor(API): update dependencies

--- api/src/Domain/EAV/Attribute/Entity/Attribute.php
+++ api/src/Domain/EAV/Attribute/Entity/Attribute.php
@@ -11,11 +11,6 @@ use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
-//Add type to attribute table
-// bin/console doctrine:migration:diff
-// bin/console doctrine:migration:migrate
-// bin/console doctrine:migration:status
-// bin/console doctrine:migration:execute 'DoctrineMigrations\Version20220113204012' --down
 #[ORM\Entity(repositoryClass: DbAttributeRepository::class)]
 final class Attribute
 {
