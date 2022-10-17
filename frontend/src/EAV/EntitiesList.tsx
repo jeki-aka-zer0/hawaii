1f32751b5109db7b784d02c005f088d4657727b0
Evgeniy Zhukov <zer0.stat@mail.ru>
2022-10-17 21:24:04 +0200

    feat(Front): add simplest entities list

--- frontend/src/EAV/EntitiesList.tsx
+++ frontend/src/EAV/EntitiesList.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+type Entity = {
+  readonly name: string
+  readonly description: string
+}
 
 const EntitiesList: React.FC = () => {
+  const [entities, setEntities] = useState<Entity[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    setLoading(true)
+    const controller = new AbortController()
+    axios.get('http://localhost:8080/eav/entity', {
+      signal: controller.signal
+    }).then(res => {
+      setLoading(false)
+      setEntities(res.data.entities)
+    })
+
+    return () => controller.abort()
+  }, [])
+
+  if (loading) {
+    return (<p>Loading...</p>)
+  }
+
   return (
-    <p>Coming soon</p>
+    <>
+      {entities.map((e: Entity) => (
+        <p key={e.name}><b>{e.name}</b>, {e.description}</p>
+      ))}
+    </>
   )
 }
 
