ea6c4cada827eb7f5808bf8d6723f7ef2eaef8d1
Evgeniy Zhukov <zer0.stat@mail.ru>
2022-10-25 21:15:36 +0200

    feat(Front): add some styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,10 @@ import EntitiesList from './EAV/EntitiesList'
 import { Link, Route, Routes } from 'react-router-dom'
 import EntityForm from './EAV/EntityForm'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <header>
         <nav>
           <ul>
             <li><Link to="/">Home</Link></li>
@@ -19,17 +19,19 @@ function App() {
             </li>
           </ul>
         </nav>
-        <Routes>
-          <Route path="/" element={<h1>Welcome to Hawaii</h1>} />
-          <Route path="/entities">
-            <Route index element={<EntitiesList />} />
-            <Route path="create" element={<EntityForm />} />
-          </Route>
-          <Route path="*" element={<h1>Page not found</h1>} />
-        </Routes>
       </header>
-    </div>
-  );
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Welcome to Hawaii</h1>}/>
+          <Route path="/entities">
+            <Route index element={<EntitiesList/>}/>
+            <Route path="create" element={<EntityForm/>}/>
+          </Route>
+          <Route path="*" element={<h1>Page not found</h1>}/>
+        </Routes>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
