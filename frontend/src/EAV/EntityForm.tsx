15043c844edabfe972187da0c7fe7ee1cf42a328
Yevgeniy Zhukov <evgeniy.zhukov.dev@gmail.com>
2023-05-08 22:03:32 +0200

    feat(Front): form submission state

--- frontend/src/EAV/EntityForm.tsx
+++ frontend/src/EAV/EntityForm.tsx
@@ -9,7 +9,7 @@ type Inputs = {
 };
 
 const EntityForm: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
+  const { register, handleSubmit, formState: { errors, isSubmitting, isDirty, isValid } } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = (data: Inputs): void => {
     console.log(data)
     axios
@@ -22,15 +22,15 @@ const EntityForm: React.FC = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" {...register("name", { required: true })} id="name" className={'size-m'}/>
-        {errors.name && <span className={"error"}>Name is required</span>}
+        <input type="text" {...register('name', { required: true, minLength: 2 })} id="name" className={'size-m'}/>
+        {errors.name && <span className={'error'}>Name is required</span>}
       </div>
       <div>
         <label htmlFor="description">Description</label>
-        <textarea {...register("description")} id="description" className={'size-m'}></textarea>
+        <textarea {...register('description')} id="description" className={'size-m'}></textarea>
       </div>
       <div>
-        <input type="submit" value="Create"/>
+        <input type="submit" value="Create" disabled={isSubmitting && isDirty && isValid}/>
       </div>
     </form>
   )
