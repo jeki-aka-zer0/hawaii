6e7f5b8d4725f4b6a0ef30197c49aab4bc822101
Yevgeniy Zhukov <evgeniy.zhukov.dev@gmail.com>
2023-05-09 21:42:31 +0200

    chore(Front): update react
    `yarn add react react-dom react@next react-dom@next` https://blog.logrocket.com/how-to-use-typescript-with-react-18-alpha/

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './css/loader.min.css';
 import './css/reset.min.css';
 import './css/index.css'
@@ -7,13 +7,14 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function
