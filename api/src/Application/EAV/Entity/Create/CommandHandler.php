af653d1c11cc5d4aafb6b3cd8856ccbc5a5f8201
Jenya Zhukov <evgeniy.zhukov.dev@gmail.com>
2023-06-13 17:54:32 +0200

    feat(Back): field exception
    Add field name as a key to the errors response if it is available

--- api/src/Application/EAV/Entity/Create/CommandHandler.php
+++ api/src/Application/EAV/Entity/Create/CommandHandler.php
@@ -8,8 +8,8 @@ use App\Domain\EAV\Entity\Entity\Entity;
 use App\Domain\EAV\Entity\Entity\EntityId;
 use App\Domain\EAV\Entity\Repository\EntityRepository;
 use App\Domain\Flusher;
+use App\Domain\Shared\Repository\FieldException;
 use DateTimeImmutable;
-use DomainException;
 
 final class CommandHandler
 {
@@ -22,8 +22,11 @@ final class CommandHandler
     public function handle(Command $command): EntityId
     {
         $name = trim($command->name);
-         if ($this->entities->hasByName(mb_strtolower($name))) {
-            throw new DomainException(sprintf('An entity with the name "%s" already exists.', $name));
+        if ($this->entities->hasByName(mb_strtolower($name))) {
+            throw FieldException::build(
+                'name' /** @see \App\Application\EAV\Entity\Create\Command::$name */,
+                sprintf('An entity with the name "%s" already exists.', $name),
+            );
         }
 
         $this->entities->add(
